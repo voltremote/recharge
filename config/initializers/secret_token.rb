4a0c26eafd3cc950df1342861dce366c26b06563
digitalWestie <giannichan@gmail.com>
2012-11-10 13:55:59 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Recharge::Application.config.secret_token = '761e70931ba22930fce4dd575f520decba4779d355f3cd4973cdf5e653f8cfe373a09b2e767875532b9b50ea8bea2fb03c14917ef67ee650967bc8b71a7523e6'
