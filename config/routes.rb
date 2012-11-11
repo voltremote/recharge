11c918350748342a93e0e9d6e77c4e0c2800e0b0
Tillmann Bielefeld <email@tielefeld.com>
2012-11-10 17:17:15 -0800

    tester graph added

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Recharge::Application.routes.draw do
   get "welcome/index"
 
   devise_for :users
+  
+  root :to => 'welcome#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -65,8 +67,7 @@ Recharge::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index' 
-  root :to => 'welcome#index'
+  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
