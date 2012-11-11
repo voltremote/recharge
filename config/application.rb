b0bd0687ce7691df219fca4a541943c7751c6a95
digitalWestie <giannichan@gmail.com>
2012-11-10 19:27:33 -0800

    added carrierwave to require

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require File.expand_path('../boot', __FILE__)
 require "devise"
 require 'rails/all'
 require 'gritter'
+require 'carrierwave'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
